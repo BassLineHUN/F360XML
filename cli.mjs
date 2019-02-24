0b9020d3c9e1e76214bee5864ad79110f2c16be5
Tamas Sallai <gsashee@gmail.com>
2019-02-24 17:41:13 +0000

    Change directory structure

--- cli.mjs
+++ cli.mjs
@@ -1,5 +1,5 @@
 import fs from "fs";
-import merger from "./merger.mjs";
+import merger from "./browser/merger.mjs";
 
 const german = fs.readFileSync("german.xml", "utf-8").split("\n").filter((a, i) => i > -1);
 const hun = fs.readFileSync("hun.xml", "utf-8").split("\n");
